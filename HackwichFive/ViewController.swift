36423bef05225dd69f5907514b408a356d41ba59
Brendan Kumasaka <bhk22@hawaii.edu>
2023-10-04 20:23:22 -1000

    more updated code

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var currentIndex = 0
+    
+    
+    var favoriteFoodsArray : [String] = ["Pizza", "Sushi", "Burgers", "Pasta", "Watermelon"]
+    
+    @IBOutlet weak var topLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        for name in favoriteFoodsArray
+        {
+            print("My favorite food is" + name)
+        }
+        
     }
-
-  
-}
+    
+    var favoriteMovies : [String] = ["TopGun", "Avengers,", "JurrasicWorld", "GuardingsoftheGalaxy", "Avatar"]
+    
+    
+    @IBAction func otherButtonPressed(_ sender: Any) {
+        
+        
+        for name in favoriteMovies
+        {
+            print("My favorite movie is" + name)
+        }
+        
+    }
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
+        
+        
+    }
+    
 
